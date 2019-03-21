c26ccb4c8f710aafca2363078ec696a14223b83a
andrea <andrea101288@hotmail.it>
2019-03-21 11:10:24 +0100

    fix date problems

--- getData.py
+++ getData.py
@@ -26,11 +26,14 @@ for eventes in et:
     event = {}
     event['startDate'] = eventes[1].text.split("T")[0]    
     event['startDateY'] = event['startDate'].split("-")[0]
-    event['startDateM'] = int(event['startDate'].split("-")[1])
-    event['startDateD'] = event['startDate'].split("-")[2] 
-    datetime_object = datetime( int(event['startDateY']), int(event['startDateM']), int(event['startDateD']))        
+    event['startDateM'] = event['startDate'].split("-")[1]
+    event['startDateD'] = event['startDate'].split("-")[2]
     
-    if datetime_object > datetime.today():        
+    datetime_object = datetime( int(event['startDateY']), int(event['startDateM']), int(event['startDateD'])) 
+    
+    
+    if datetime_object > datetime.now():
+    
         event['nid'] = eventes[15].text
         event['title'] = eventes[0].text.split(">")[1].split("<")[0]
         result = firebase.put('/events/' + event['nid'], 'name' , event['title'])
@@ -39,19 +42,19 @@ for eventes in et:
         event['endDate'] = eventes[2].text.split("T")[0]
         if event['startDate'] != event['endDate'] :    
             result = firebase.put('/events/' + event['nid'], 'endDate' , event['endDate']) 
-        event['picture'] = eventes[3]
+        #event['picture'] = eventes[3]
+        #result = firebase.put('/events/' + event['nid'], 'picture' , event['picture'])
         event['place'] = eventes[6].text
         result = firebase.put('/events/' + event['nid'], 'place' , event['place'])
         event['prize'] = eventes[7].text
         result = firebase.put('/events/' + event['nid'], 'prize' , event['prize'])
-        #event['city'] = eventes[1][4].text
-        #result = firebase.put('/events/' + eventes[0].text, 'city' , event['city'])
+        event['meetingPoint'] = eventes[9].text
+        result = firebase.put('/events/' + event['nid'], 'meetingPoint' , event['meetingPoint'])
         #event['country'] = eventes[1][13].text
         #result = firebase.put('/events/' + eventes[0].text, 'country' , event['country'])
         #event['date'], event['time'] = (eventes[3].text).split('-')
         #result = firebase.put('/events/' + eventes[0].text, 'date' , event['date'])
-        #result = firebase.put('/events/' + eventes[0].text, 'time' , event['time'])
-    
-    eventsArray.append(event)
+        #result = firebase.put('/events/' + eventes[0].text, 'time' , event['time'])    
+        eventsArray.append(event)
     
 print(eventsArray) 
