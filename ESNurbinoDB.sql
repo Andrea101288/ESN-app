9ec0fb24075d8d9ba7c54cd330894ee2b84c894b
andrea <andrea101288@hotmail.it>
2019-03-22 20:41:55 +0100

    fix Db connection problems and events' data storage

--- ESNurbinoDB.sql
+++ ESNurbinoDB.sql
@@ -16,13 +16,14 @@ data_nascita varchar(20) NOT NULL
 
 CREATE TABLE Events(
 
-name varchar(20) PRIMARY KEY,
-startDate varchar(20) NOT NULL,
-startTime varchar(20) NOT NULL,
-endDate varchar(20) ,
-EndTime varchar(20) ,
-place varchar(20),
-prize int(100),
-meetingPoint varchar(20)
+nid int(100) PRIMARY KEY,
+name varchar(50) NOT NULL,
+startDate varchar(50) NOT NULL,
+startTime varchar(50) NOT NULL,
+endDate varchar(50) ,
+EndTime varchar(50) ,
+place varchar(50),
+prize varchar(50),
+meetingPoint varchar(50)
 
 ) Engine = InnoDB;
--- getData.py
+++ getData.py
@@ -11,12 +11,27 @@ from datetime import datetime
 import time
 import MySQLdb
 
+myHost='localhost'
+myUser='root'
+myPasswd='Jago2009'
+myDb='esnurbino'
+myCharSet = 'utf8'
 
-conn = MySQLdb.connect(host='localhost',user='root',passwd='Jago2009',db='esnurbino')
-cur = conn.cursor()
-
-event = {}
-eventsArray = []
+def insertEventDb(sqlHost, sqlUser, sqlPasswd, sqlDb, eventId, eventName, eventStartDate, eventStartTime, eventEndDate, eventEndTime, eventPlace, eventPrize, eventMeetingPoint):
+    
+    conn = MySQLdb.connect(host=sqlHost,user=sqlUser,passwd=sqlPasswd,db=sqlDb,charset=myCharSet)
+    cur = conn.cursor()
+    
+    try:
+        cur.execute("USE "+ sqlDb)
+        sql = "INSERT INTO events VALUES (" + eventId  + "," + eventName + "," + eventStartDate + "," + eventStartTime + "," + eventEndDate + "," + eventEndTime + "," + eventPlace + "," + eventPrize + "," + eventMeetingPoint + ");"
+        cur.execute(sql)
+        conn.commit()
+        conn.close()
+    except:
+        print("ERRORE NELL INSERIMENTO DEI DATI")
+        conn.close()
+        
 try:
     result = requests.get("https://www.esnurbino.it/api/v1/events.xml", verify = False)
 except:
@@ -26,45 +41,50 @@ et = ET.fromstring(result.text)
         
 for eventes in et:
     event = {}
-    event['startDate'] = eventes[1].text.split("T")[0]     
-    event['startDateY'] = event['startDate'].split("-")[0]
-    event['startDateM'] = event['startDate'].split("-")[1]
-    event['startDateD'] = event['startDate'].split("-")[2]
+    startDate = event['startDate'] = eventes[1].text.split("T")[0]     
+    startDateY = event['startDate'].split("-")[0]
+    startDateM = event['startDate'].split("-")[1]
+    startDateD = event['startDate'].split("-")[2]
     
-    datetime_object = datetime( int(event['startDateY']), int(event['startDateM']), int(event['startDateD'])) 
+    datetime_object = datetime( int(startDateY), int(startDateM), int(startDateD)) 
     
     
-    if datetime_object > datetime.now():
+    #if datetime_object > datetime.now():
         
-        nid = event['nid'] = eventes[15].text
-        name = event['title'] = eventes[0].text.split(">")[1].split("<")[0]
-        endDate = event['endDate'] = eventes[2].text.split("T")[0]
-        startTime = event['startTime'] = eventes[1].text.split("T")[1].split("+")[0]
-        endTime = event['endTime'] = eventes[2].text.split("T")[1].split("+")[0]
+    nid = event['nid'] = eventes[15].text
+    name = event['title'] = eventes[0].text.split(">")[1].split("<")[0]
+    endDate = event['endDate'] = eventes[2].text.split("T")[0]
+    startTime = event['startTime'] = eventes[1].text.split("T")[1].split("+")[0]
+    endTime = event['endTime'] = eventes[2].text.split("T")[1].split("+")[0]
+    place = event['place'] = eventes[6].text
+    prize = event['prize'] = eventes[7].text
+    meetingPoint = event['meetingPoint'] = eventes[9].text
+    
+    nid = "'" + str(int(nid)) + "'"
+    name = "'" + name + "'"
+    startDate = "'" + startDate + "'"
+    startTime = "'" + startTime + "'"
+    endTime = "'" + endTime + "'"        
+    
+    if event['startDate'] == event['endDate'] :  
+        endDate = "NULL" 
+    else:
+        endDate = "'" + endDate + "'"
         
-        if event['startDate'] == event['endDate'] :  
-            endDate = NULL            
-        #event['picture'] = eventes[3]
-        place = event['place'] = eventes[6].text
-        if event['place'] == "" :
-            place = NULL
-        prize = event['prize'] = eventes[7].text
-        if event['prize'] == "" :
-            prize = NULL
-        meetingPoint = event['meetingPoint'] = eventes[9].text
-        if event['meetingPoint'] == "" :
-            meetingPoint = NULL
-        try:
-            cur.execute('USE esnurbino')
-            sql = "INSERT INTO events ( name , startDate, startTime, endDate, endTime, place, prize, meetingPoint ) VALUES({},{},{},{},{},{},{},{})"
-            val = (name , startDate, startTime, endDate, endTime, place, prize, meetingPoint) 
-            cur.execute(sql, val)
-            conn.commit()
-            
-        except:
-            print("ERRORE NELL INSERIMENTO DEI DATI")
-        time.sleep(1)
-        conn.close()
-        
-        # eventsArray.append(event)
+    if event['place'] == None :
+        place = "NULL"
+    else:  
+        place = "'" + place + "'"
+    if event['prize'] == None :
+        prize = "NULL"
+    else:
+        prize = "'" + str(prize) + "'"
+    if event['meetingPoint'] == None :
+        meetingPoint = "NULL"
+    else:
+        meetingPoint = "'" + meetingPoint + "'"
+    
+    insertEventDb(myHost, myUser, myPasswd, myDb, nid, name, startDate, startTime, endDate, endTime, place, prize, meetingPoint)
+   
+    
     
