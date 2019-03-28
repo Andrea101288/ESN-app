d22821ad1062877a8d6bb37940486df487656581
Radeox <dawid.weglarz95@gmail.com>
2019-03-28 16:30:22 +0100

    Changed connection open/close managment

--- ESN-Database/manager.py
+++ ESN-Database/manager.py
@@ -32,6 +32,9 @@ class Manager:
 
     def insert_event(self, nid, name, start_date, start_time, end_date, end_time, place, price, meeting_point):
         """Insert events in the database"""
+        # Connect to DB
+        self.connect()
+
         try:
             # Prepare query
             query = "INSERT INTO event VALUES({0}, '{1}', '{2}', '{3}',\
@@ -54,9 +57,15 @@ class Manager:
             else:
                 print("Unknown error! Exiting...")
                 raise e
+        finally:
+            # Close connection
+            self.close()
 
     def insert_user(self, email, password, name, surname, birthdate):
         """Insert user in the database"""
+        # Connect to DB
+        self.connect()
+
         try:
             # Prepare query
             query = "INSERT INTO erasmusUser VALUES('{0}', '{1}', '{2}', '{3}', '{4}')".format(email,
@@ -75,9 +84,15 @@ class Manager:
             else:
                 print("Unknown error! Exiting...")
                 raise e
+        finally:
+            # Close connection
+            self.close()
 
     def get_password_hash(self, email):
         """Check credentials in the database and return result"""
+        # Connect to DB
+        self.connect()
+
         # Prepare query
         query = "SELECT password FROM erasmusUser WHERE email='{0}'".format(email)
 
@@ -88,11 +103,17 @@ class Manager:
         for password_hash in self.cursor:
             return password_hash[0]
 
+        # Close connection
+        self.close()
+
         # User not found or wrong password
         return None
 
     def get_events(self, offset=0, limit=10):
         """Return last 'limit' events, eventually with an offset"""
+        # Connect to DB
+        self.connect()
+
         # Return value
         rv = []
 
@@ -105,5 +126,8 @@ class Manager:
         # Check result
         for event in self.cursor:
             rv.append(event)
+
+        # Close connection
+        self.close()
 
         return rv
