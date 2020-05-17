ac01b069e2f2f5d97e74ec04dd22a86e504ff22e
Andrea Mancini <36378695+Andrea101288@users.noreply.github.com>
2020-05-17 23:57:14 +0200

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -3,9 +3,9 @@ import * as firebase from 'firebase'
 export default class Firebase {
     static init() {
         firebase.initializeApp({
-            apiKey: "AIzaSyDavx19z8FXliXsu-5RuHIKC9eN0AO74Jc",
-            authDomain: "internetservicesandapplication.firebaseapp.com",
-            databaseURL: "https://internetservicesandapplication.firebaseio.com",
+            apiKey: "<apikey>",
+            authDomain: "FireBaseDomainName",
+            databaseURL: "<FireBAseDatabseURL>",
             projectId: "internetservicesandapplication",
             storageBucket: "internetservicesandapplication.appspot.com",
         })
